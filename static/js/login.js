d64115188f3ee6f99b7bc4389e14987ef43a1fb1
kyraluo <luozishan97@gmail.com>
2024-04-28 00:55:44 +0800

    Add the register page, base on the login style, they are using same style sheet and same js file. some js function to check the user's password input, to make sure the user's input is acceptable.

--- static/js/login.js
+++ static/js/login.js
@@ -1,29 +1,19 @@
-//提示密码长度过大或者过小
+//tell user the input Pin is too long or too short
 function myfunction(){
-    var mess=document.getElementsByClassName('message')[0];
-    var input1=document.querySelector('.PIN1');
+    const mess=document.getElementsByClassName('message')[0];
+    const input1=document.querySelector('.PIN1');
     if(input1.value.length<6){
       mess.innerHTML='Password should between 6 and 20';
       mess.style.color='blue';
     }else if(input1.value.length>20){
-        mess.innerHTML='Password is too long, make sure no more than 16 digit';
+        mess.innerHTML='Password is too long, make sure no more than 20 digit';
         mess.style.color='darkgoldenrod';
     }else{
-        mess.innerHTML='It is ok';
+        mess.innerHTML='Good Password';
         mess.style.color='black';
     }
 }
 // hid or not hid the psw
-function HID(){
-    const pin=document.querySelector('.PIN0');
-    if(pin.type==="password"){
-        pin.type='text';
-    }else{
-        pin.type='password'
-    }
-}
-
-
 
 function Hid(className) {
     const pin = document.querySelector('.' + className);
@@ -39,10 +29,10 @@ function checkpin() {
     var mess = document.getElementsByClassName('message1')[0];
     var PIN1 = document.querySelector('.PIN1');
     var PIN2 = document.querySelector('.PIN2');
-    if (PIN1.value != PIN2.value) {
+    if (PIN1.value !== PIN2.value) {
         mess.innerHTML = 'Two times password is not same';
         mess.style.color = 'red';
-    } else if (PIN1.value = PIN2.value) {
+    } else if (PIN1.value === PIN2.value) {
         mess.innerHTML = 'The two password are same';
         mess.style.color = 'blue';
     }
