d02aba0bf2b041cce90375bdcd003583b1277362
kyraluo <luozishan97@gmail.com>
2024-04-27 23:28:58 +0800

    Since I am Writing the html under the flask, so I have an app.py file to make sure I can run the html

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template,jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+app.config['DEBUG'] = True
+@app.route('/')  # 这里是新添加的路由，表示根路径
+def index():
+    return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')  # 确保有 login.html 文件
+    else:  # 处理 POST 请求
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == "expected_username" and password == "expected_password":
+            return jsonify(status='success')
+        else:
+            return jsonify(status='fail', error='Invalid credentials')
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
