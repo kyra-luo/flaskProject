2fa97d3f1d7584b6ae9eb1c630af48d10b6698f9
WenjieS <22470722@student.uwa.edu>
2024-05-01 02:22:41 +0800

    adding word count for the topic textarea

--- app/form.py
+++ app/form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class PostForm(FlaskForm):
-    title = TextAreaField('Your title', validators=[
-        DataRequired(), Length(min=1, max=80)])
+    communities = SelectField('Communities', choices=[('1', 'Community 1'), ('2', 'Community 2'), ('3', 'Community 3')], validators=[DataRequired()])
+    topic = TextAreaField('Your topic', validators=[
+        DataRequired(), Length(min=1, max=100)])
     post = TextAreaField('Say something...', validators=[
         DataRequired(), Length(min=1, max=1000)])
