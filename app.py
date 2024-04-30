738a7dc8b97adf6a43548acbb5e41c350174959f
WenjieS <22470722@student.uwa.edu>
2024-04-30 22:09:53 +0800

    try flask to see if rendered propoly and test Wtforms on create.html

--- app.py
+++ app.py
@@ -1,35 +1,12 @@
-from flask import Flask, request, render_template,jsonify
+from flask import Flask
 
-app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.config['DEBUG'] = True
-@app.route('/')  # 这里是新添加的路由，表示根路径
-def index():
-    return render_template('index.html')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')  # 确保有 login.html 文件
-    else:  # 处理 POST 请求
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == "expected_username" and password == "expected_password":
-            return jsonify(status='success')
-        else:
-            return jsonify(status='fail', error='Invalid credentials')
+app = Flask(__name__)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template('register.html')
 
-@app.route('/base',methods=['GET', 'POST'])
-def base():
-    return render_template('base.html')
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
-
-
