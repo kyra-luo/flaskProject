597df2be9aae139a103f7b7b934171df8699a765
WenjieS <22470722@student.uwa.edu>
2024-05-08 01:59:51 +0800

    minor change the _post and create part on css and script

--- app/form.py
+++ app/form.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 class PostForm(FlaskForm):
     communities = SelectField('Communities', choices=[('1', 'Community 1'), ('2', 'Community 2'), ('3', 'Community 3')], validators=[DataRequired()])
     topic = TextAreaField('Your topic', validators=[
-        DataRequired(), Length(min=1, max=200)])
+        DataRequired(), Length(min=1, max=250)])
     post = TextAreaField('Say something...', validators=[
         DataRequired(), Length(min=1, max=5000)])
     
     
 class CommentForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[
-        DataRequired(), Length(min=1, max=400)])
+        DataRequired(), Length(min=1, max=1000)])
