62ec86c593c535c4768d4a3167271a6251378dc5
Nachuan Wang <adrianwang76@gmail.com>
2024-05-12 18:03:13 +0800

    fix merge issues

--- app/form.py
+++ app/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
 
 
