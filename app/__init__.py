47ac534f696e734521f9ad47ff1b796f7e0574ad
WenjieS <22470722@student.uwa.edu>
2024-05-13 15:51:40 +0800

    Implement the moment js for time showing

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 # from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 # csrf = CSRFProtect(app)
 #inital the login
 login = LoginManager(app)
