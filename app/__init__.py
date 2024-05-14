603636ee3867064c51826b2569c2fd60292ec353
WenjieS <22470722@student.uwa.edu>
2024-05-14 14:12:54 +0800

    have a try

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
-from config import Config
+from config import Config, DevelopmentConfig, TestingConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 # from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 # csrf = CSRFProtect(app)
 #inital the login
 login = LoginManager(app)
-print(app.config['SQLALCHEMY_DATABASE_URI'])
+login.login_view = 'login'
+# print(app.config['SQLALCHEMY_DATABASE_URI'])
 
 from app import routes, models
 #
--- config.py
+++ config.py
@@ -5,5 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
