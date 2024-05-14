59a2ebd849ae606c7898cd4d77abc997c4d8697a
WenjieS <22470722@student.uwa.edu>
2024-05-15 04:31:05 +0800

    got blueprint doing its work

--- app/__init__.py
+++ app/__init__.py
@@ -6,18 +6,27 @@ from flask_login import LoginManager
 from flask_moment import Moment
 from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-moment = Moment(app)
-# csrf = CSRFProtect(app)
-#inital the login
-login = LoginManager(app)
-login.login_view = 'login'
-print(app.config['SQLALCHEMY_DATABASE_URI'])
 
-from app import routes, models
+db = SQLAlchemy()
+moment = Moment()
+login=LoginManager()
+login.login_view = 'main.login'
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    from app.blueprint import main
+    app.register_blueprint(main)
+    db.init_app(app)
+    # migrate = Migrate(app, db)
+    moment.init_app(app)
+    # csrf = CSRFProtect(app)
+    login.init_app(app)
+    
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
+    return app
+
+from app import models
 #
 # with app.app_context():
 #     db.create_all()
--- flask_project.py
+++ flask_project.py
@@ -1,7 +1,13 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db
+from app import db, create_app
+from flask_migrate import Migrate
 from app.models import User, Post
+from config import DevelopmentConfig as D
+
+app = create_app(D)
+migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
